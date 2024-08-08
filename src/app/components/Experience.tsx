7fe169bbd5cb0d03cb8f21da25bd89981b41a3c2
Vinicin1101 <vserafim_o@outlook.com>
2024-08-08 18:07:54 -0300

    Experience page

---
+++ src/app/components/Experience.tsx
@@ -0,0 +1,31 @@
+interface tag {
+    type: string,
+    value: string
+}
+
+interface experience {
+    enterprise: string,
+    role: string,
+    tags: Array<tag>,
+    desc: string
+}
+
+const ExperienceComponent = ({exp}: { exp: experience }) => {
+    return (
+        <div className="experiences flex flex-col p-2 my-4">
+            <h4 className="text-2xl">{ exp.enterprise }</h4>
+            <p className="font-light text-slate-500 text-sm">
+                { exp.role }
+            </p>
+            <ul className="tags flex flex-wrap mb-2 mt-2 gap-3 p-0">
+                {exp.tags.map((tag: tag) => 
+                    <li key={tag.value} className={`${tag.type} badge badge-primary btn buttom-secondary font-light pl-2 pr-2 pt-1 pb-1`}>
+                        { tag.value }
+                    </li>
+                )}
+            </ul>
+            <p>{ exp.desc }</p>
+        </div>
+    )
+}
+export default ExperienceComponent;
---
+++ src/app/experiences/page.tsx
@@ -0,0 +1,28 @@
+import ExperienceComponent from "../components/Experience";
+
+interface expObj {
+    enterprise: string,
+    role: string,
+    desc: string,
+    tags: Array<any>
+}
+
+const Experiences = async () => {
+    const response = await fetch("https://fringe-obsidian-louse.glitch.me/my/experiences");
+    const experiences: Array<any> = await response.json();
+
+    return (
+        <main className="flex flex-col items-center">
+            <article className="container max-w-screen-md">
+                <section className="col-md-8 offset-md-2 mt-3">
+                    <h2 className="text-center text-3xl text-semibold my-8 mb-10">Experiências</h2>
+                    {experiences.map((exp: expObj) =>
+                        <ExperienceComponent exp={exp} />
+                    )}
+                </section>
+            </article>
+        </main>
+    )
+}
+
+export default Experiences;
